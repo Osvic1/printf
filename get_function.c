9f98ed1c819b80a74a8e1bc0a21e5707c121f996
Festus Omaku <fomaku3@gmail.com>
2022-11-19 17:47:00 +0100

    restructuring

--- get_function.c
+++ get_function.c
@@ -9,29 +9,20 @@
 int (*collect_func(const char *str, int id))(va_list)
 {
 	prt func[] = {
-
-{"c", print_char}, {"s", print_str}, {"%", print_percentage}, {NULL, NULL}
-
-
+		{"c", print_char}, {"s", print_str},
+		{"%", print_percentage}, {NULL, NULL}
 	};
 
-int v = 0, j = 0;
+	int v = 0, j = 0;
 
-if (str)
-
+	if (str)
 	{
-
-while (func[v].func != NULL)
-
+		while (func[v].func != NULL)
 		{
-
-if (func[v].c[j] == str[id])
-
-return (func[v].func);
-
-v++;
+			if (func[v].c[j] == str[id])
+				return (func[v].func);
+			v++;
 		}
 	}
-
 	return (NULL);
 }
