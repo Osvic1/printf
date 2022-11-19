d7915ae58eb4bc16ab1d598978a85009f959bd31
Osvic1 <timothyv952@gmail.com>
2022-11-19 07:50:51 -0800

    update

--- get_function.c
+++ get_function.c
@@ -10,8 +10,32 @@ int (*collect_func(const char *str, int id))(va_list)
 {
 	prt func[] = {
 		{"c", print_char}
+
 {"s" , print_str};
+
 {"%" , print_percent};
+
+
 		{NULL, NULL}
 	};
+
+int v = 0, j = 0;
+
+if (str)
+
+	{
+
+while (prt[v].func != NULL)
+
+		{
+
+if (prt[v].c[j] == str[id])
+
+return (prt[v].func);
+
+v++;
+		}
+	}
+
+	return (NULL);
 }
