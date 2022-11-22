f24b907e9e883139a0726e277259dd96bdafc867
Osvic1 <timothyv952@gmail.com>
2022-11-22 08:31:03 -0800

    Hot fix

--- _printf.c
+++ _printf.c
@@ -1,58 +1,132 @@
-/*
- * File: _printf.c
- * Auth: Omaku Festus Friday
- *       Timothy Victor
- */
-
 #include "main.h"
 
 /**
- *_printf - replicates the standard function of printf
- *@format: parameter containing char and format specifiers to print out
- *Return: all characters passed in right format
+ * t_char - print a character
+ *@va:character
  *
+ * Return: no return
  */
+int t_char(va_list va)
+{
+	int c;
 
-int _printf(const char *format, ...)
+	c = va_arg(va, int);
+	_putchar(c);
+	return (1);
+}
+/**
+ * t_string - print a string
+ *@va: pointer to string
+ *
+ * Return: no return
+ */
+int t_string(va_list va)
 {
 	int i, j;
-	va_list lst;
-	int (*p_func)(va_list);
+	char n[] = "(null)";
+	char *s = va_arg(va, char *);
 
+	if (s == NULL)
+	{
+		for (i = 0; n[i] != '\0'; i++)
+			_putchar(n[i]);
+		return (6);
+	}
+	for (j = 0; s[j] != '\0'; j++)
+		_putchar(s[j]);
+	return (j);
+}
+/**
+ * print_number - Entry point
+ *@va: the integer to print
+ * Return: no return
+ */
+int print_number(va_list va)
+{
+	int i, len, r, l;
+	unsigned int abs, num, numt;
+	int n = va_arg(va, int);
+
+	len = 0;
 	i = 0;
-	j = 0;
-
-	if (!format || (format[i] == '%' && format[i + 1] == '\0'))
+	r = 1;
+	l = 1;
+	if (n < 0)
 	{
+		_putchar('-');
+		len++;
+		abs = -n;
+	} else
+	{
+		abs = n;
+	}
+
+	num = abs;
+	while (num > 0)
+	{
+		num /= 10;
+		i++;
+	}
+
+	while (r < i)
+	{
+		l *= 10;
+		r++;
+	}
+	while (l >= 1)
+	{
+		numt = (abs / l) % 10;
+		_putchar(numt + '0');
+		len++;
+		l /= 10;
+	}
+	return (len);
+}
+
+/**
+ * _printf - print output according to a format
+ *@format: first argument
+ *
+ * Return: the number of characters printed(excluding the null byte)
+ */
+int _printf(const char *format, ...)
+{
+	int i = 0, j, len = 0, count;
+	va_list valist;
+	types difftypes[] = {{'c', t_char}, {'s', t_string}, {'d', print_number},
+			     {'i', print_number}, {'b', binary}, {'u', print_unsigned},
+			     {'x', hexa}, {'X', hexa_upper}, {'o', octal}, {'R', print_rot},
+			     {'r', print_rev}, {'S', stringhexa}, {'p', pointer}};
+
+	if (format == NULL || (format[0] == '%' && format[1] == 0))
 		return (-1);
-	}
-	if (!format[i])
+	va_start(valist, format);
+	while (format != NULL && format[i])
 	{
-		return (0);
-	}
-
-	va_start(lst, format);
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		if (format[i] == '%')
+		if (format[i] != '%')
+			len += _putchar(format[i]);
+		else
 		{
-			if (format[i + 1] == '\0')
-				return (-1);
-			p_func = collect_func(format, i + 1);
-			if (p_func == NULL)
+			i++;
+			if (format[i] == '%')
+				len += _putchar('%');
+			j = 0;
+			count = 0;
+			while (j < 13)
 			{
+				if (format[i] == difftypes[j].t)
+				{
+					len += difftypes[j].f(valist);
+					count = 1;
+					break; }
+				j++; }
+			if (!count && format[i] != '%')
+			{
+				len++;
+				len++;
 				_putchar('%');
-				j++;
-			}
-			else
-			{
-				j = j + p_func(lst);
-				i += collect_flags(format, i + 1);
-			}
-		} else
-		{
-			_putchar(format[i]);
-			j++;
-		}
-	} return (j);
+				_putchar(format[i]); }}
+		i++; }
+	va_end(valist);
+	return (len);
 }
--- main.h
+++ main.h
@@ -1,57 +1,39 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef _MAIN_H
+#define _MAIN_H
 
-#include <limits.h>
-#include <string.h>
+#include <stdarg.h>
 #include <unistd.h>
 #include <stdio.h>
-#include <stdarg.h>
+#include <limits.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 /**
- *struct print - proto struct
- *@c: test parameter
- *@func: function to  print data with corresponding specifier
- */
-typedef struct print
+* struct types_arguments - Struct op
+*
+* @t: The operator
+* @f: The function associated
+*/
+typedef struct types_arguments
 {
-	/* for all formats*/
-	char *c;
-	int (*func)(va_list);
-} prt;
-
-int (*collect_func(const char *, int))(va_list);
-int collect_flags(const char *, int);
-/* prototypes for specifier functions */
-int _printf(const char *format, ...);
+	char t;
+	int (*f)(va_list va);
+} types;
 int _putchar(char c);
-int print_char(va_list);
-int print_str(va_list);
-int print_percentage(va_list);
-int print_num(va_list);
-int print_binary(va_list);
-int print_octadex(va_list);
-int print_unsigned_intg(va_list);
-int print_hex(va_list);
-int print_hexx(va_list);
-int print_S(va_list);
-int print_address(va_list);
-int print_HEX(long int, unsigned int, unsigned int);
-int print_plus_sign(va_list);
-int print_space(va_list);
-int print_hash_HEX(va_list);
-int print_hash_octal(va_list);
-int print_hash_hex(va_list);
-int print_long_num(va_list);
-int print_short_num(va_list);
-int print_long_octal(va_list);
-int print_short_octal(va_list);
-int print_long_hexx(va_list);
-int print_short_hexx(va_list);
-int print_long_hex(va_list);
-int print_short_hex(va_list);
-int print_long_unsigned_intg(va_list);
-int print_short_unsigned_intg(va_list);
+int _strlen(char *s);
+int print_number(va_list va);
+int binary(va_list va);
+int print_unsigned(va_list va);
+int hexa(va_list va);
+int hexa_upper(va_list va);
+int octal(va_list va);
+int print_rot(va_list va);
+char *malloc_str(char *str);
+char *rot13(char *p);
+int print_rev(va_list va);
+int hexa_special(unsigned int c);
+int stringhexa(va_list va);
+int pointer(va_list va);
+int hexa_pointer(unsigned long int c);
+int _printf(const char *format, ...);
 
 #endif
